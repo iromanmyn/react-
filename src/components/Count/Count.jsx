ce2d508c5cd4dd7e3e83b1d603e38cd017a5f40b
iromanmyn <872490574@qq.com>
2020-04-29 21:44:13 +0800

    gengxin

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 import store from '../../redux/store'
+import {
+    createIncrementAction,
+    createDecrementAction
+} from '../../redux/count_action_creator'
 export default class Count extends Component {
-
-    state = {
-        count:0
-    }
 
     increment = ()=> {
         //1.获取用户的输入
         const {value} = this.refs.user_selected
         //2.通知redux加value
-        store.dispatch({type:'increment',data:value*1})
+        store.dispatch(createIncrementAction(value*1))
     }
     decrement = ()=> {
         //1.获取用户的输入
         const {value} = this.refs.user_selected
-         //2.通知redux减value
-         store.dispatch({type:'decrement',data:value*1})
+        //2.通知redux减value
+        store.dispatch(createDecrementAction(value*1))
         
     }
     //当前和是奇数再加
@@ -26,7 +26,7 @@ export default class Count extends Component {
         //2.获取现在的和
         const count = store.getState()
         if(count%2 === 1){
-            store.dispatch({type:'increment',data:value*1})
+            store.dispatch(createIncrementAction(value*1))
         }
     }
     //等500毫秒再加
@@ -35,7 +35,7 @@ export default class Count extends Component {
         const {value} = this.refs.user_selected
        
         setTimeout(() => {
-            
+            store.dispatch(createIncrementAction(value*1))
         }, 500);
     }
 
--- src/redux/count_reducer.js
+++ src/redux/count_reducer.js
@@ -1,16 +1,17 @@
 //该文件是真正操作状态的人
 //该函数是counteReducer,action是动作对象
+import {INCREMENT,DECREMENT} from './action_type'
 let initState = 0//定义初始状态
 export default function (preState=initState,action){
     // if(preSatae === undefined) preSatae = 0
     const {type,data} = action
     let newState
     switch (type) {
-        case 'increment':
+        case INCREMENT:
             //如果加
             newState = preState + data
             return newState
-        case 'decrement':
+        case DECREMENT:
             //如果减
             newState = preState - data
             return newState
