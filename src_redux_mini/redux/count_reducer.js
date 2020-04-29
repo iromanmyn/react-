2d09a4569bbffa16cd27dd2ae818a0edda995fe6
iromanmyn <872490574@qq.com>
2020-04-29 20:37:32 +0800

    geng

---
+++ src_redux_mini/redux/count_reducer.js
@@ -0,0 +1,23 @@
+//该文件是真正操作状态的人
+//该函数是counteReducer,action是动作对象
+let initState = 0//定义初始状态
+export default function (preState=initState,action){
+    // if(preSatae === undefined) preSatae = 0
+    const {type,data} = action
+    let newState
+    switch (type) {
+        case 'increment':
+            //如果加
+            newState = preState + data
+            return newState
+        case 'decrement':
+            //如果减
+            newState = preState - data
+            return newState
+        default:
+            //如果动作类型type不是加也不是减，就是初始化的时候
+            return preState
+    }
+
+    
+}
